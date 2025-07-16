1efdcd5d1720c6d046699837cbf9d075722a90c9
Behz92 <ahmedbahaa2004.ab@gmail.com>
2025-07-16 06:36:10 +0300

    img and form update

--- src/app/login/testimonials/create/page.tsx
+++ src/app/login/testimonials/create/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import '../update/[name]/form.css';
+import '../update/[id]/form.css';
 
 export default function CreateTestimonial() {
   const router = useRouter();
