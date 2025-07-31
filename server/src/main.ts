37e059e48bfdb03d657545017d8a5646acb1ccde
omar hossam <153917786+OMARhossamSAMI@users.noreply.github.com>
2025-07-31 06:02:45 +0300

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -27,6 +27,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:3001',
       'https://leaderscollege.up.railway.app',
+      'https://leaders-frontend.onrender.com'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
