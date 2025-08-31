e6060646e8f1f0b5ebd324254aa3c4d788ff7ebc
youssefelsahhar <youssefsahhar2406@gmail.com>
2025-08-31 17:00:40 +0300

    bookings & slots edits ddone

--- server/src/booktour/booktour.module.ts
+++ server/src/booktour/booktour.module.ts
@@ -4,9 +4,11 @@ import { BookTourController } from './booktour.controller';
 import { BookTourService } from './booktour.service';
 import { BookTourSlot, BookTourSlotSchema } from '../Schemas/booktour-slot.schema';
 import { BookTourBooking, BookTourBookingSchema } from '../Schemas/booktour-booking.schema';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     MongooseModule.forFeature([
       { name: BookTourSlot.name, schema: BookTourSlotSchema },
       { name: BookTourBooking.name, schema: BookTourBookingSchema },
